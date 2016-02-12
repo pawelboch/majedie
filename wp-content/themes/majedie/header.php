94fec305ba245cd34a209d9496e2084476859816
alexns1518 <alexp@nurtureagency.com>
2016-02-12 09:41:22 +0100

    update

--- wp-content/themes/majedie/header.php
+++ wp-content/themes/majedie/header.php
@@ -5,6 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
 		<meta name="robots" content="noindex, nofollow">
 		<title><?php wp_title('|', true, 'right'); ?></title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
@@ -13,7 +14,7 @@
 				<h1><a href="/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-fs8.png" alt="Majedie"></a></h1>
 				<nav>
 					<ul class="wpg-top-menu">
-						<li class="wpg-dropdown-menu"><label for="wpg-dropdown-consultant">Consultant</label>
+						<li class="wpg-dropdown-menu"><label for="wpg-dropdown-consultant">Consultant <i class="fa fa-angle-down"></i></label>
 							<input type="checkbox" id="wpg-dropdown-consultant">
 							<ul class="sub-menu">
 								<li><a href="#">Example 1</a></li>
@@ -21,7 +22,7 @@
 								<li><a href="#">Example 3</a></li>
 							</ul>
 						</li>
-						<li class="wpg-dropdown-menu"><label for="wpg-dropdown-lang">United Kingdom</label>
+						<li class="wpg-dropdown-menu"><label for="wpg-dropdown-lang">United Kingdom <i class="fa fa-angle-down"></i></label>
 							<input type="checkbox" id="wpg-dropdown-lang">
 							<ul class="sub-menu">
 								<li><a href="#">French</a></li>
@@ -37,7 +38,7 @@
 				<nav>
 					<ul class="wpg-other-menu">
 						<li><a href="#">Majlq Login</a></li>
-						<li><input type="submit" value="Submit"><input type="search" placeholder="Search"></li>
+						<li><input type="submit" value="Submit" class="wpg-ico-search"><input type="search" placeholder="Search"></li>
 					</ul>
 					<?php wp_nav_menu( array( 'container' => fasle, 'menu' => 'main', 'menu_class' => 'wpg-main-menu' ) ); ?>
 				</nav>
