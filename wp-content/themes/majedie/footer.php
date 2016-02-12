363a41feffa0bad3c5c1bfe9c929580e3d7a7563
alexns1518 <alexp@nurtureagency.com>
2016-02-12 14:57:33 +0100

    update

--- wp-content/themes/majedie/footer.php
+++ wp-content/themes/majedie/footer.php
@@ -2,10 +2,12 @@
 	<footer class="wpg-main-footer">
 		<div class="container">
 			<div class="row">
-				<div class="col-xl-8">
+				<div class="col-xs-12 col-sm-8">
 					<p>&copy; MAJEDIE ASSET MANAGEMENT LTD 2015 Registered in England no. 04446767, VAT no. GB882408116<br>Authorised and regulated by the Financial Conduct Authority</p>	
 				</div>
-				<div class="col-xl-4">.col-md-1</div>
+				<div class="col-xs-12 col-sm-4">
+					footer
+				</div>
 			</div>			
 			
 		</div>
