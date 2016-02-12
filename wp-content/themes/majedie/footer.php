c4aa821e12f9275077fe1202e2cc178796fe2f6f
alexns1518 <alexp@nurtureagency.com>
2016-02-12 16:31:44 +0100

    hmm

--- wp-content/themes/majedie/footer.php
+++ wp-content/themes/majedie/footer.php
@@ -2,16 +2,16 @@
 	<footer class="wpg-main-footer">
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-12 col-sm-7 col-md-8">
-					<p>&copy; MAJEDIE ASSET MANAGEMENT LTD 2015 Registered in England no. 04446767, VAT no. GB882408116<br>Authorised and regulated by the Financial Conduct Authority</p>	
-				</div>
-				<div class="col-xs-12 col-sm-5 col-md-4">
-					<ul class="wpg-socials-ico">
+				<div class="col-xs-12 col-sm-5 col-md-4 pull-sm-right pull-md-right">
+					<ul class="wpg-socials-ico pull-xs-left pull-sm-right pull-md-right">
 						<li class="wpg-social-ico-facebook"><a href="#"><span>Facebook</span> <i class="fa fa-facebook"></i></a></li>
 						<li class="wpg-social-ico-linkedin"><a href="#"><span>Linkedin</span> <i class="fa fa-linkedin"></i></a></li>
 						<li class="wpg-social-ico-twitter"><a href="#"><span>Twitter</span> <i class="fa fa-twitter"></i></a></li>
 						<li class="wpg-social-ico-y-play"><a href="#"><span>Youtube Play</span> <i class="fa fa-youtube-play"></i></a></li>
 					</ul>
+				</div>
+				<div class="col-xs-12 col-sm-7 col-md-8">
+					<p>&copy; MAJEDIE ASSET MANAGEMENT LTD 2015 Registered in England no. 04446767, VAT no. GB882408116<br>Authorised and regulated by the Financial Conduct Authority</p>	
 				</div>
 			</div>			
 			
