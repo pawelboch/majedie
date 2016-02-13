955b7fea2a27e58ca0a55d290e722c1e158db54e
unknown <bnowak@nurtureagency.com>
2016-02-13 12:43:21 +0100

    Pagebox page template

---
+++ wp-content/themes/majedie/templates/page-pagebox.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+Template Name: Pagebox
+*/
+
+?>
+
+<?php get_header(); ?>
+
+<div class="main">
+	<div class="container-fluid">
+		<div class="container">
+            <?php pagebox() ;?>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?> 
