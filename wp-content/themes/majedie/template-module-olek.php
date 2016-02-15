c9834b721bebfead0422d563babb184388f8b2e1
alexns1518 <alexp@nurtureagency.com>
2016-02-15 13:40:09 +0100

    update

---
+++ wp-content/themes/majedie/template-module-olek.php
@@ -0,0 +1,41 @@
+<?php
+/* Template Name: module example */
+?>
+<?php get_header(); ?>
+
+<div class="module-full-width-text-slider bg-grey-grain">
+	<?php $idUniqid=uniqid(); ?>
+	<div class="container">
+		<p>Majedie was founded by a team from Mercury Asset Management with a clear aim: </p>
+		<h2>simply to make money for our clients.</h2>
+		
+		<div class="cycle-slideshow-wrap">
+			<div class="cycle-slideshow"
+				data-cycle-fx="scrollHorz"
+				data-cycle-swipe="true"
+				data-cycle-swipe-fx="scrollHorz"
+				data-cycle-pager=".cycle-slideshow-pager-<?php echo $idUniqid; ?>"
+				data-cycle-slides="> div"
+				data-cycle-prev=".cycle-slideshow-prev-<?php echo $idUniqid; ?>"
+				data-cycle-next=".cycle-slideshow-next-<?php echo $idUniqid; ?>"
+			>
+				<div>
+					<p>We are proud of our distinctive, long-term track recordbut remain as hungry to perform today as we were on day one.</p>
+				</div>
+				<div>
+					<p>We are proud of our distinctive, long-term track recordbut remain as hungry to perform today as we were on day one.</p>
+				</div>
+				<div>
+					<p>We are proud of our distinctive, long-term track recordbut remain as hungry to perform today as we were on day one.</p>
+				</div>
+			</div>
+			<div class="cycle-slideshow-prev cycle-slideshow-prev-<?php echo $idUniqid; ?>"><i class="fa fa-angle-left"></i></div>
+			<div class="cycle-slideshow-next cycle-slideshow-next-<?php echo $idUniqid; ?>"><i class="fa fa-angle-right"></i></div>
+		</div>
+
+		<div class="cycle-slideshow-pager cycle-slideshow-pager-<?php echo $idUniqid; ?>"></div>
+
+	</div>
+</div>
+
+<?php get_footer(); ?> 
