017474798cfbecefcdd73a5946a73ae097676085
wojciechlampa <w.lampa@designcrown.co.uk>
2016-02-15 14:14:32 +0100

    pagebox template | fix

--- wp-content/themes/majedie/pagebox/templates/full-width/template.php
+++ wp-content/themes/majedie/pagebox/templates/full-width/template.php
@@ -1,12 +1,4 @@
-<?php 
-global $post; 
-$post_id = $wp_query->post->ID
-
-$post = get_post( $post_id );
-$slug = $post->post_name;
-?>
-
-<div class="main-container <?php echo $slug ;?>">
+<div class="main-container">
 	<?php foreach ($this->get_variable( 'full_width_modules' ) as $module): ?>
 	    <?php $module->display(); ?>
 	<?php endforeach ?>
