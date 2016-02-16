5ccb8e2a8a315580e9003de6a1b2c5e7bc13ee90
alexns1518 <alexp@nurtureagency.com>
2016-02-16 12:42:03 +0100

    up

--- wp-content/themes/majedie/pagebox/modules/full width contact form + title + paragraph/views/default.php
+++ wp-content/themes/majedie/pagebox/modules/full width contact form + title + paragraph/views/default.php
@@ -1,24 +1,26 @@
 <div class="module-full-width-contact-form-title-paragraph">
 	<div class="container">
-		<h2>Contact Us</h2>
-		<p>If you have any queries of a non-urgent nature, please fill in the form and we will endeavour to get back to you within 48 hours.</p>
-		<p>Alternatively to speak to someone about dealing for direct investment, please call  BNY Mellon on +44 (0)344 892 0974.</p>
-		<?php echo do_shortcode('[contact-form-7 id="24" title="Contact form 1"]'); ?>
-		<?php
-/*
-<div class="row">
-<div class="col-xs-12 col-sm-6">
-<label for="wpg-contact-name">Name</label>[text* name id:wpg-contact-name]
-</div>
-<div class="col-xs-12 col-sm-6">
-<label for="wpg-contact-email">Email</label>[email* email id:wpg-contact-email]
-</div>
-<div class="col-md-12 wpg-clear-both">
-<label for="wpg-comments">Comments</label>[textarea* comments id:wpg-comments]
-</div>
-<div>[submit "SUBMIT"]</div>
-</div>
-*/
-		?>
+		<div>
+			<h2>Contact Us</h2>
+			<p>If you have any queries of a non-urgent nature, please fill in the form and we will endeavour to get back to you within 48 hours.</p>
+			<p>Alternatively to speak to someone about dealing for direct investment, please call  BNY Mellon on +44 (0)344 892 0974.</p>
+			<?php echo do_shortcode('[contact-form-7 id="24" title="Contact form 1"]'); ?>
+			<?php
+	/*
+	<div class="row">
+	<div class="col-xs-12 col-sm-6">
+	<label for="wpg-contact-name">Name</label>[text* name id:wpg-contact-name]
+	</div>
+	<div class="col-xs-12 col-sm-6">
+	<label for="wpg-contact-email">Email</label>[email* email id:wpg-contact-email]
+	</div>
+	<div class="col-md-12 wpg-clear-both">
+	<label for="wpg-comments">Comments</label>[textarea* comments id:wpg-comments]
+	</div>
+	<div>[submit "SUBMIT"]</div>
+	</div>
+	*/
+			?>
+		</div>
 	</div>
 </div>
