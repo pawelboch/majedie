7d42d606e77a7779f4e3e108abc35f0d7cd22e39
alexns1518 <alexp@nurtureagency.com>
2016-02-17 12:04:17 +0100

    up

--- wp-content/themes/majedie/template-module-o.php
+++ wp-content/themes/majedie/template-module-o.php
@@ -74,9 +74,9 @@
 <div class="module-two-blocks-with-logo-title-button-background-image">
 	<div class="container container-corrected">
 
-		<div class="row">
+		<div class="row wpg-wrap-items-max-height">
 			<div class="col-xs-12 col-sm-6">
-				<div class="wpg-red-block span-table" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/cloud-big-fs8.png)">
+				<div class="wpg-red-block span-table wpg-item-max-height" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/cloud-big-fs8.png)">
 					<div class="span-table-row">
 						<div class="span-table-cell vertical-align-top">
 							<h2><img src="<?php echo get_template_directory_uri(); ?>/assets/images/cloud-fs8.png" alt="x"><span>Maj<strong>IQ</strong></span></h2>
@@ -92,7 +92,7 @@
 				</div>
 			</div>
 			<div class="col-xs-12 col-sm-6">
-				<div class="wpg-blue-block span-table" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/one-fs8.png)">
+				<div class="wpg-blue-block span-table wpg-item-max-height" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/one-fs8.png)">
 					<div class="span-table-row">
 						<div class="span-table-cell vertical-align-bottom">
 							<p>We have one clear Aim:<br>Simply to make money<br>for our Clients</p>
@@ -119,6 +119,28 @@
 
 
 	</div>
+</div>
+
+<div class="container">
+
+	<div class="row wpg-wrap-items-max-height">
+		<div class="col-xs-12 col-sm-4">
+			<div class="wpg-red-block span-table wpg-item-max-height" style="background-color: red;">
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-4">
+			<div class="wpg-red-block span-table wpg-item-max-height" style="background-color: lime;">
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-4">
+			<div class="wpg-red-block span-table wpg-item-max-height" style="background-color: yellow;">
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+			</div>
+		</div>
+	</div>
+
 </div>
 
 <?php get_footer(); ?> 
