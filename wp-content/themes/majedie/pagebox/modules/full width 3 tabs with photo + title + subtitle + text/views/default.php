53422625ebee093de0249d25c3acf6eb86dccc64
Tobiasz Gerczuk <kontakt@tobit.com.pl>
2016-02-26 11:28:16 +0100

    Module full width 3 tabs with photo + title + subtitle + text   +   Module full width vertical 2 blocks with post + block with twitter post (New Content)

--- wp-content/themes/majedie/pagebox/modules/full width 3 tabs with photo + title + subtitle + text/views/default.php
+++ wp-content/themes/majedie/pagebox/modules/full width 3 tabs with photo + title + subtitle + text/views/default.php
@@ -1,3 +1,42 @@
-<div class="container-fluid module_full_width_3_tabs_with_photo-title-subtitle-text">
+<div class="module-full-width-3-tabs-with-photo-title-subtitle-text">
+	<?php $idUniqid=uniqid(); ?>
+	<div class="container">
+		<div class="row pager-mfw3twptst pager-mfw3twptst-<?php echo $idUniqid; ?>" data-wpg-equal-height-wrap="height"></div>
+		
+		<div 
+			class="cycle-slideshow wpg-main-slides-persons"
+			data-cycle-fx="scrollHorz"
+    		data-cycle-timeout="0"
+    		data-cycle-speed="600"
+    		data-cycle-manual-speed="300"
+    		data-cycle-swipe="true"
+    		data-cycle-swipe-fx="scrollHorz"
+    		data-cycle-auto-height="container"
+			data-cycle-slides="> div"
+			data-cycle-pager=".pager-mfw3twptst-<?php echo $idUniqid; ?>"
+		>
+				<?php foreach ($this->get('teams') as $team) : ?>
 
+					<?php 
+
+					 ?>
+
+			<?php	
+				$image_attributes = wp_get_attachment_image_src( $attachment_id = $team->image, 'team' );
+		    ?>
+			<?php /* tu trzeba dociac obrazek w atrybucie data: 301x355px */ ?>
+			<div data-cycle-pager-template="<div class='col-xs-4'><div class='pager-mfw3twptst-item' data-wpg-equal-height-item><img src='<?php  echo $image_attributes[0]; ?> ' alt='x'><h3><?php echo $team->title; ?></h3><p class='wpg-job-title'><?php echo $team->bottom_title; ?></p></div></div>">
+				<div class="row">
+					<div class="col-xs-12 col-md-10">
+						<h3><?php echo $team->title; ?></h3>
+						<p class="wpg-job-title"><?php echo $team->bottom_title; ?></p>
+						<?php echo $team->editor; ?>
+
+					</div>
+				</div>
+			</div>
+			<?php endforeach ?>
+		</div>
+
+	</div>
 </div>
--- wp-content/themes/majedie/pagebox/modules/full width vertical 2 blocks with post + block with twitter post/class-module.php
+++ wp-content/themes/majedie/pagebox/modules/full width vertical 2 blocks with post + block with twitter post/class-module.php
@@ -52,19 +52,214 @@ class Module extends Abstract_Module {
 			// minimum and maximum percent width that module fits in
 			// WPGeeks_Forms
 			'fields'      => array(
-				array(
-					'type'        => 'text',
-					'group'       => __( 'General', 'pagebox' ),
-					'name'        => 'title',
-					'label'       => __( 'Title', 'pagebox' ),
-					'description' => __( 'Main module title', 'pagebox' ),
-				),
+			   array(
+                                         'type'        => 'post',
+                                        'name'        => 'post1',
+                                        'group'		  => __('Post1', 'pagebox'),
+                                        'label'       => __( 'Post 1', 'pagebox' ),
+                                        'description' => __( 'Choose post 1 to display', 'pagebox' ),
+                                ),
+
+                    array(
+                                         'type'        => 'post',
+                                        'name'        => 'post2',
+                                        'group'		  => __('Post2', 'pagebox'),
+                                        'label'       => __( 'Post 2', 'pagebox' ),
+                                        'description' => __( 'Choose post 1 to display', 'pagebox' ),
+                                ),
 
 				array(
 					'type'        => 'colorpicker',
 					'group'		    => __( 'Design', 'pagebox' ),
-					'name'        => 'button_font_color',
-					'label'       => __( 'Button font color', 'pagebox' ),
+					'name'        => 'post1_background_color',
+					'label'       => __( 'Post 1 background color', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+
+				array(
+					'type'        => 'number',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post1_category_size',
+					'label'       => __( 'Post 1Category size', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'colorpicker',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post1_category_color',
+					'label'       => __( 'Post 1 category color', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'number',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post1_title_size',
+					'label'       => __( 'Post 1 title size', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'colorpicker',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post1_title_color',
+					'label'       => __( 'Post 1 title color', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'number',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post1_date_size',
+					'label'       => __( 'Post 1 date size', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'colorpicker',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post1_date_color',
+					'label'       => __( 'Post 1 date color', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'number',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post1_paragraph_size',
+					'label'       => __( 'Post 1 paragraph size', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'colorpicker',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post1_paragraph_color',
+					'label'       => __( 'Post 1 paragraph color', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+
+
+
+
+
+
+
+
+						array(
+					'type'        => 'colorpicker',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post2_background_color',
+					'label'       => __( 'Post 2 background color', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+
+				array(
+					'type'        => 'number',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post2_category_size',
+					'label'       => __( 'Post 2 Title size', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'colorpicker',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post2_category_color',
+					'label'       => __( 'Post 2 category color', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'number',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post2_title_size',
+					'label'       => __( 'Post 2 title size', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'colorpicker',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post2_title_color',
+					'label'       => __( 'Post 2 title color', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'number',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post2_date_size',
+					'label'       => __( 'Post 2 date size', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'colorpicker',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post2_date_color',
+					'label'       => __( 'Post 2 date color', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'number',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post2_paragraph_size',
+					'label'       => __( 'Post 2 paragraph size', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'colorpicker',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'post2_paragraph_color',
+					'label'       => __( 'Post 2 paragraph color', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+
+
+
+
+
+
+
+
+						array(
+					'type'        => 'colorpicker',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'block3_background_color',
+					'label'       => __( 'Block 3 background color', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+
+				array(
+					'type'        => 'number',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'block3_above_text_size',
+					'label'       => __( 'Block 3 above title - text size', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'colorpicker',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'block3_above_text_color',
+					'label'       => __( 'third block above text color', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'number',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'block_text_size',
+					'label'       => __( 'Third block text size', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'colorpicker',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'block_text_color',
+					'label'       => __( 'Third block text color', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'number',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'block3_link_size',
+					'label'       => __( 'Third block link size', 'pagebox' ),
+					'description' => __( '', 'pagebox' ),
+				),
+				array(
+					'type'        => 'colorpicker',
+					'group'		    => __( 'Design', 'pagebox' ),
+					'name'        => 'block3_link_color',
+					'label'       => __( 'third block link color', 'pagebox' ),
 					'description' => __( '', 'pagebox' ),
 				),
 			)
