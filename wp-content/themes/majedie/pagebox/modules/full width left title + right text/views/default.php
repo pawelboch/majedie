58ab63797db75d5a4cd421551304ec37ab664f92
wojciechlampa <w.lampa@designcrown.co.uk>
2016-02-26 13:15:31 +0100

    fix #6 | module update

--- wp-content/themes/majedie/pagebox/modules/full width left title + right text/views/default.php
+++ wp-content/themes/majedie/pagebox/modules/full width left title + right text/views/default.php
@@ -1,52 +1,44 @@
-
 <div class="module-full-width-left-title-right-text wpg-bg-grain"
 
 <?php	if($this->get('background_color')) {
-		echo 'background-color: ' . $this->get('background_color') . ';';
-				}
-			;?>
+echo 'style="';
+echo 'background-color: ' . $this->get('background_color') . ';';
+echo '"';
+	}
+;?>
 >
 	<div class="container">
 		<div class="wpg-container-inset">
 			<?php if($this->get('text_left') != '') { ;?>
-						<h2
-						<?php if($this->get('text_left_size') || $this->get('text_left_color') != '') {
-							echo 'style=" ';
-							if($this->get('text_left_size') != '') {
-								echo 'font-size: ' . $this->get('text_left_size') . 'px;';
-							}
-							if($this->get('text_left_color') != '') {
-								echo 'color: ' . $this->get('text_left_color') . ';';
-							}
-							echo '"';
-						} ;?>
-						><?php echo $this->get('text_left') ;?></h2>
-						<?php } ;?>
-			
+				<h2
+				<?php if($this->get('text_left_size') || $this->get('text_left_color') != '') {
+					echo 'style=" ';
+					if($this->get('text_left_size') != '') {
+						echo 'font-size: ' . $this->get('text_left_size') . 'px;';
+					}
+					if($this->get('text_left_color') != '') {
+						echo 'color: ' . $this->get('text_left_color') . ';';
+					}
+					echo '"';
+				} ;?>
+				>
+				<?php echo $this->get('text_left') ;?>
+				</h2>
+				<?php } ;?>
 			<?php if($this->get('text_right') != '') { ;?>
-						<p
-						<?php if($this->get('text_right_size') || $this->get('text_right_color') != '') {
-							echo 'style=" ';
-							if($this->get('text_right_size') != '') {
-								echo 'font-size: ' . $this->get('text_right_size') . 'px;';
-							}
-							if($this->get('text_left_color') != '') {
-								echo 'color: ' . $this->get('text_right_color') . ';';
-							}
-							echo '"';
-						} ;?>
-						><?php echo $this->get('text_right') ;?></p>
-						<?php } ;?>
-			<!--
-			<div class="row">
-				<div class="col-xs-12 col-sm-5">
-					<h2>£11bn</h2>
-				</div>
-				<div class="col-xs-12 col-sm-7">
-					<p>Assets under management<br>total approximately</p>
-				</div>
-			</div>
-			-->
+				<p
+				<?php if($this->get('text_right_size') || $this->get('text_right_color') != '') {
+					echo 'style=" ';
+					if($this->get('text_right_size') != '') {
+						echo 'font-size: ' . $this->get('text_right_size') . 'px;';
+					}
+					if($this->get('text_left_color') != '') {
+						echo 'color: ' . $this->get('text_right_color') . ';';
+					}
+					echo '"';
+				} ;?>
+				><?php echo $this->get('text_right') ;?></p>
+				<?php } ;?>
 		</div>
 	</div>
 </div>
--- wp-content/themes/majedie/pagebox/modules/full width search funds + title + paragraph + link/views/default.php
+++ wp-content/themes/majedie/pagebox/modules/full width search funds + title + paragraph + link/views/default.php
@@ -1,16 +1,82 @@
-<div class="module-full-width-search-funds-title-paragraph-link">
+<div class="module-full-width-search-funds-title-paragraph-link"
+<?php	if($this->get('background_outer_color')) {
+echo 'style="';
+echo 'background-color: ' . $this->get('background_outer_color') . ';';
+echo '"';
+	}
+;?>
+>
 	<div class="container">
 
 		<div class="span-table">
-			<div class="span-table-cell vertical-align-middle wpg-first-block">
-				<h2><?php echo $this->get('title') ;?></h2>
-				<div class="text">
-					<?php echo $this->get('text') ;?>
-				</div>
-				<a href="<?php echo $this->get('button_url') ;?>" class="btn-2"><?php echo $this->get('button_text') ;?></a>
+			<div class="span-table-cell vertical-align-middle wpg-first-block"
+			<?php	if($this->get('background_inner_color')) {
+			echo 'style="';
+			echo 'background-color: ' . $this->get('background_inner_color') . ';';
+			echo '"';
+				}
+			;?>
+			>
+				<h2
+				<?php if($this->get('title_size') || $this->get('title_color') != '') {
+					echo 'style=" ';
+					if($this->get('title_size') != '') {
+						echo 'font-size: ' . $this->get('title_size') . 'px;';
+					}
+					if($this->get('title_color') != '') {
+						echo 'color: ' . $this->get('title_color') . ';';
+					}
+					echo '"';
+				} ;?>
+				><?php echo $this->get('title') ;?></h2>
+				<p class="text"
+				<?php if($this->get('text_size') || $this->get('text_color') != '') {
+					echo 'style=" ';
+					if($this->get('text_size') != '') {
+						echo 'font-size: ' . $this->get('text_size') . 'px;';
+					}
+					if($this->get('text_color') != '') {
+						echo 'color: ' . $this->get('text_color') . ';';
+					}
+					echo '"';
+				} ;?>
+				>
+				<?php echo $this->get('text') ;?>
+				</p>
+				<a href="<?php echo $this->get('button_url') ;?>" class="btn-2"
+				<?php if($this->get('button_size') || $this->get('button_color') != '') {
+					echo 'style=" ';
+					if($this->get('button_size') != '') {
+						echo 'font-size: ' . $this->get('button_size') . 'px;';
+					}
+					if($this->get('button_color') != '') {
+						echo 'color: ' . $this->get('button_color') . ';';
+						echo 'border-color: ' . $this->get('button_color') . ';';
+					}
+					echo '"';
+				} ;?>
+				><?php echo $this->get('button_text') ;?></a>
 			</div>
-			<div class="span-table-cell vertical-align-middle wpg-second-block">
-				<h2><?php echo $this->get('fund_form_title') ;?></h2>
+			<div class="span-table-cell vertical-align-middle wpg-second-block"
+			<?php	if($this->get('background_right_block_color')) {
+			echo 'style="';
+			echo 'background-color: ' . $this->get('background_right_block_color') . ';';
+			echo '"';
+				}
+			;?>
+			>
+				<h2
+				<?php if($this->get('fund_form_title_size') || $this->get('fund_form_title_color') != '') {
+					echo 'style=" ';
+					if($this->get('fund_form_title_size') != '') {
+						echo 'font-size: ' . $this->get('fund_form_title_size') . 'px;';
+					}
+					if($this->get('fund_form_title_color') != '') {
+						echo 'color: ' . $this->get('fund_form_title_color') . ';';
+					}
+					echo '"';
+				} ;?>
+				><?php echo $this->get('fund_form_title') ;?></h2>
 				<form method="get" action="/">
 					<select class="wpg-plugin-select">
 						<option value="1">Fund</option>
@@ -45,7 +111,19 @@
 						<option value="5">Option 5</option>
 						<option value="6">Option 6</option>
 					</select>
-					<button class="btn-1" type="submit" name="search-funds" value=""><?php echo $this->get('fund_form_button_text') ;?></button>
+					<button class="btn-1" type="submit" name="search-funds" value=""
+					<?php if($this->get('fund_form_button_size') || $this->get('fund_form_button_color') != '') {
+						echo 'style=" ';
+						if($this->get('fund_form_button_size') != '') {
+							echo 'font-size: ' . $this->get('fund_form_button_size') . 'px;';
+						}
+						if($this->get('fund_form_button_color') != '') {
+							echo 'color: ' . $this->get('fund_form_button_color') . ';';
+							echo 'border-color: ' . $this->get('fund_form_button_color') . ';';
+						}
+						echo '"';
+					} ;?>
+					><?php echo $this->get('fund_form_button_text') ;?></button>
 				</form>
 				
 			</div>
@@ -53,3 +131,11 @@
 
 	</div>
 </div>
+<?php	if($this->get('background_right_block_color')) {
+echo '<style scoped>';
+echo '.module-full-width-search-funds-title-paragraph-link .wpg-second-block::before {';
+echo 'border-color: transparent ' . $this->get('background_right_block_color') . ' transparent transparent;';
+echo '}';
+echo '</style>';
+			}
+;?>
