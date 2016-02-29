9097ff28d0e4a83e3e4ea946c1271cf13d2ede0c
Tobiasz Gerczuk <kontakt@tobit.com.pl>
2016-02-29 14:17:14 +0100

    full_width_2_blocks_with_title-paragraph

--- wp-content/themes/majedie/pagebox/modules/full width 2 blocks with title + paragraph/views/default.php
+++ wp-content/themes/majedie/pagebox/modules/full width 2 blocks with title + paragraph/views/default.php
@@ -1,3 +1,62 @@
-<div class="container-fluid module_full_width_2_blocks_with_title-paragraph">
-
+<div class="module-full-width-2-blocks-with-title-paragraph">
+	<div class="container">
+		<div class="row" data-wpg-equal-height-wrap="min-height">
+			<div class="col-xs-12 col-sm-6">
+				<div class="wpg-inset-col" style="
+<?php	if($this->get('title1_background_color')) {
+		echo 'background-color: ' . $this->get('title1_background_color') . ';';
+				}
+			;?>
+" data-wpg-equal-height-item data-wpg-equal-height-item-smartphone-remove-height>
+					<?php if($this->get('title1') != '') { ;?>
+						<h2
+						<?php if($this->get('title1_size') || $this->get('title1_font_color') != '') {
+							echo 'style=" ';
+							if($this->get('title1_size') != '') {
+								echo 'font-size: ' . $this->get('title1_size') . 'px;';
+							}
+							if($this->get('title1_font_color') != '') {
+								echo 'color: ' . $this->get('title1_font_color') . ';';
+							}
+							echo '"';
+						} ;?>
+						><?php echo $this->get('title1') ;?></h2>
+						<?php } ;?>
+					<?php if($this->get('editor1') != '') {
+						
+			echo $this->get('editor1') ;
+						
+						} ?>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6">
+				<div class="wpg-inset-col" style="
+<?php	if($this->get('title2_background_color')) {
+		echo 'background-color: ' . $this->get('title2_background_color') . ';';
+				}
+			;?>
+" data-wpg-equal-height-item data-wpg-equal-height-item-smartphone-remove-height>
+					<?php if($this->get('title2') != '') { ;?>
+						<h2
+						<?php if($this->get('title2_size') || $this->get('title2_font_color') != '') {
+							echo 'style=" ';
+							if($this->get('title2_size') != '') {
+								echo 'font-size: ' . $this->get('title2_size') . 'px;';
+							}
+							if($this->get('title1_font_color') != '') {
+								echo 'color: ' . $this->get('title2_font_color') . ';';
+							}
+							echo '"';
+						} ;?>
+						><?php echo $this->get('title2') ;?></h2>
+						<?php } ;?>
+					<?php if($this->get('editor2') != '') {
+						
+			echo $this->get('editor2') ;
+						
+						} ?>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
--- wp-content/themes/majedie/pagebox/modules/full width timeline/views/default.php
+++ wp-content/themes/majedie/pagebox/modules/full width timeline/views/default.php
@@ -1,3 +1,42 @@
-<div class="container-fluid module_full_width_timeline">
-
+<div class="module-full-width-timeline">
+	<div class="container">
+		<ul class="wpg-timeline">
+			<li>
+				<h3>14/10/2015</h3>
+				<p>Majedie limits UK Income Fund capacity</p>
+			</li>
+			<li>
+				<h3>10/09/2015</h3>
+				<p>Majedie named Active Manager of the Year</p>
+			</li>
+			<li>
+				<h3>13/07/2015</h3>
+				<p>Majedie Asset Management scoops five industry awards</p>
+			</li>
+			<li>
+				<h3>09/07/2015</h3>
+				<p>Majedie US Equity Fund reaches $100m AUM on first anniversary</p>
+			</li>
+			<li>
+				<h3>02/07/2015</h3>
+				<p>Majedie Global Equity and Global Focus mark first anniversary</p>
+			</li>
+			<li>
+				<h3>21/05/2015</h3>
+				<p>Majedie Asset Management wins Active UK Equities Award</p>
+			</li>
+			<li>
+				<h3>03/03/2015</h3>
+				<p>Majedie Asset Management wins two fund performance awards</p>
+			</li>
+			<li>
+				<h3>22/01/2015</h3>
+				<p>Majedie US Equity Fund races out of the traps</p>
+			</li>
+			<li>
+				<h3>14/01/2015</h3>
+				<p>Majedie UK Income Fund marks third anniversary at the top</p>
+			</li>
+		</ul>
+	</div>
 </div>
