f37b5d7cb82d7562747ded0934dd869d94de83b4
alexns1518 <alexp@nurtureagency.com>
2016-02-29 06:38:34 +0100

    up

--- wp-content/themes/majedie/template-module-o.php
+++ wp-content/themes/majedie/template-module-o.php
@@ -1632,6 +1632,61 @@
 
 <hr class="wpg-separator">
 
+<div class="module-full-width-fund-content">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-md-8 wpg-inset-col">
+				<div class="wpg-inset-box-1">
+					<div class="wpg-large-font-size">
+						<p>
+							The Fund is a bottom up, high conviction stock picking fund based on a process that has been honed over Adrian’s 20 year investment career. We target stocks offering an attractive asymmetric return profile with clear catalysts to realise the potential. That is where we believe there is solid downside protection through asset backing or robust earnings streams whilst at the same time offering significant share price upside from both valuation rerating and future earnings surprises.
+						</p>
+					</div>
+					<p>This process has produced top-decile performance in US equities over the past eight years and fits seamlessly into the range of Majedie’s top performing funds that are based on similar investment principles</p>
+					
+					<div class="wpg-video-box">
+						<iframe src="https://player.vimeo.com/video/47111178?color=999999&amp;title=0&amp;byline=0&amp;portrait=0" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+					</div>
+
+					<div class="wpg-inset-box-again">
+						<p>
+							For more information, please contact Mark Hepburn<br>
+							Telephone: 020 7618 3970<br>
+							Email: <a href="mailto:mark.hepburn@majedie.com">mark.hepburn@majedie.com</a>
+						</p>
+						 
+						<p>For documentation relating to the US Equity Fund, please click <a href="#">here</a>.</p>
+					</div>
+				</div>
+
+			</div>
+			<div class="col-xs-12 col-md-4 wpg-inset-col">
+				<div class="wpg-inset-box-2">
+					<div class="wpg-table-wrap">
+						<table>
+							<thead>
+								<tr><th>Performance</th><th>Fund</th><th>Share Class</th></tr>
+							</thead>
+							<tbody>
+								<tr><td>3 months</td><td>7.4</td><td>A8.6</td></tr>
+								<tr><td>3 months</td><td>7.4</td><td>A8.6</td></tr>
+								<tr><td>3 months</td><td>7.4</td><td>A8.6</td></tr>
+								<tr><td>3 months</td><td>7.4</td><td>A8.6</td></tr>
+							</tbody>
+						</table>
+					</div>
+					<div class="footnotes"><p>*As at 31 December 2015</p></div>
+					<p>Inception Date: June 26th 2014</p>
+					<p>Benchmark: S&amp;P 500 (Net)</p>
+					<p>These figures are representative of the</p>
+					<p>net of fees Z USD share class</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<hr class="wpg-separator">
 <!-- .col-xs-	.col-sm-	.col-md-	.col-lg-	.col-xl- -->
 <!--
 data-wpg-equal-height-wrap="min-height"
