f8035899881d1230f2fd6b843da57f9ae1eed1f1
wojciechlampa <w.lampa@designcrown.co.uk>
2016-03-01 10:13:03 +0100

    update

--- wp-content/themes/majedie/pagebox/modules/full width vertical 2 blocks with post + block with twitter post/views/default.php
+++ wp-content/themes/majedie/pagebox/modules/full width vertical 2 blocks with post + block with twitter post/views/default.php
@@ -1,56 +1,35 @@
-		<?php
+<?php
 $post1 = $this->get('post1');
-$p1 = $post1->post; 
-
+$p1 = $post1->post;
 $post2 = $this->get('post2');
-$p2 = $post2->post; 
-
+$p2 = $post2->post;
 ?>
-
-
-		<div class="col-xs-12 col-md-4">
-
-
-
-
-				<!-- Module full width vertical 2 blocks with post + block with twitter post (New Content) -->
-				<div class="module-full-width-vertical-2-blocks-with-post-block-with-twitter-post">
-
-							<?php 
-
+<div class="col-md-12">
+	<!-- Module full width vertical 2 blocks with post + block with twitter post (New Content) -->
+	<div class="module-full-width-vertical-2-blocks-with-post-block-with-twitter-post">
+		<?php
 		$args = array(
 		'post_type' => 'post',
 		'p'  =>  $p1,
 		);
-
 		$args2 = array(
 		'post_type' => 'post',
 		'p'  =>  $p2,
 		);
-
-
- 
-$the_query = new WP_Query( $args ); ?>
-
-<?php if ( $the_query->have_posts() ) : ?>
-
-
-<!-- POST 1 -->
-
-
-	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-
-
-					<div class="row">
-						<div class="col-xs-12">
-							<div class="wpg-post-box wpg-bg-navy-blue" style="
-<?php	if($this->get('post2_background_color')) {
-		echo 'background-color: ' . $this->get('post2_background_color') . ';';
-				}
-			;?>
-" >
-
-								<?php if($post1) { ;?>
+		
+		$the_query = new WP_Query( $args ); ?>
+		<?php if ( $the_query->have_posts() ) : ?>
+		<!-- POST 1 -->
+		<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="wpg-post-box wpg-bg-navy-blue" style="
+					<?php	if($this->get('post2_background_color')) {
+						echo 'background-color: ' . $this->get('post2_background_color') . ';';
+								}
+					;?>
+					" >
+					<?php if($post1) { ;?>
 					<p class="wpg-post-box_tag"
 						<?php if($this->get('post1_category_color') || $this->get('post1_category_size') != '') {
 							echo 'style=" ';
@@ -62,49 +41,38 @@ $the_query = new WP_Query( $args ); ?>
 							}
 							echo '"';
 						} ;?>
-						><?php the_category(', ') ?></p>
-						<?php } ;?>
-								<h3><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
-							<p class="wpg-post-box_date"><?php echo the_date('d/m/Y'); ?></p>
-								<div class="wpg-post-box_main-content">
-									<p>	<?php 
+					><?php the_category(', ') ?></p>
+					<?php } ;?>
+					<h3><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
+					<p class="wpg-post-box_date"><?php echo the_date('d/m/Y'); ?></p>
+					<div class="wpg-post-box_main-content">
+						<p>	<?php
 						echo get_the_excerpt(); ?></p>
-								</div>
-								
-							</div>
-						</div>
 					</div>
-
-						<?php endwhile; ?>
-			<?php wp_reset_postdata(); ?>
-
-	<?php else : ?>
+				</div>
+			</div>
+		</div>
+		<?php endwhile; ?>
+		<?php wp_reset_postdata(); ?>
+		<?php else : ?>
 		<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-	<?php endif; ?>
-
-<!-- END  POST 1 -->
-
-
-<!-- POST 2 -->
-			<?php
-
-			$the_query = new WP_Query( $args2 ); ?>
-
-<?php if ( $the_query->have_posts() ) : ?>
-
-
-	<!-- the loop -->
-			<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-
-					<div class="row">
-						<div class="col-xs-12">
-							<div class="wpg-post-box wpg-bg-white" style="
-<?php	if($this->get('post1_background_color')) {
-		echo 'background-color: ' . $this->get('post1_background_color') . ';';
-				}
-			;?>
-" 								<?php if($post2) { ;?>
-								<p class="wpg-post-box_tag"
+		<?php endif; ?>
+		<!-- END  POST 1 -->
+		<!-- POST 2 -->
+		<?php
+		$the_query = new WP_Query( $args2 ); ?>
+		<?php if ( $the_query->have_posts() ) : ?>
+		<!-- the loop -->
+		<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="wpg-post-box wpg-bg-white" style="
+					<?php	if($this->get('post1_background_color')) {
+						echo 'background-color: ' . $this->get('post1_background_color') . ';';
+								}
+					;?>
+					" 								<?php if($post2) { ;?>
+					<p class="wpg-post-box_tag"
 						<?php if($this->get('post2_category_color') || $this->get('post2_category_size') != '') {
 							echo 'style=" ';
 							if($this->get('post2_category_size') != '') {
@@ -115,10 +83,10 @@ $the_query = new WP_Query( $args ); ?>
 							}
 							echo '"';
 						} ;?>
-						><?php the_category(', ') ?></p>
-						<?php } ;?>
-							<h3>
-						<?php if($post2) { ;?>
+					><?php the_category(', ') ?></p>
+					<?php } ;?>
+					<h3>
+					<?php if($post2) { ;?>
 					<a href="<?php echo get_permalink(); ?>"
 						<?php if($this->get('post2_title_color') || $this->get('post2_title_size') != '') {
 							echo 'style=" ';
@@ -130,8 +98,8 @@ $the_query = new WP_Query( $args ); ?>
 							}
 							echo '"';
 						} ;?>
-						><?php echo get_the_title(); ?></a>
-						<?php } ;?>
+					><?php echo get_the_title(); ?></a>
+					<?php } ;?>
 					</h3>
 					<?php if($post2) { ;?>
 					<p class="wpg-post-box_date"
@@ -145,57 +113,46 @@ $the_query = new WP_Query( $args ); ?>
 							}
 							echo '"';
 						} ;?>
-						><?php echo the_date('d/m/Y'); ?></p>
-						<?php } ;?>
-								<div class="wpg-post-box_main-content">
-									<?php if($post2) { ;?>
-					<p
-						<?php if($this->get('post2_paragraph_color') || $this->get('post2_paragraph_size') != '') {
-							echo 'style=" ';
-							if($this->get('post2_paragraph_size') != '') {
-								echo 'font-size: ' . $this->get('post2_paragraph_size') . 'px;';
-							}
-							if($this->get('post2_paragraph_color') != '') {
-								echo 'color: ' . $this->get('post2_paragraph_color') . ';';
-							}
-							echo '"';
-						} ;?>
-						><?php 
+					><?php echo the_date('d/m/Y'); ?></p>
+					<?php } ;?>
+					<div class="wpg-post-box_main-content">
+						<?php if($post2) { ;?>
+						<p
+							<?php if($this->get('post2_paragraph_color') || $this->get('post2_paragraph_size') != '') {
+								echo 'style=" ';
+								if($this->get('post2_paragraph_size') != '') {
+									echo 'font-size: ' . $this->get('post2_paragraph_size') . 'px;';
+								}
+								if($this->get('post2_paragraph_color') != '') {
+									echo 'color: ' . $this->get('post2_paragraph_color') . ';';
+								}
+								echo '"';
+							} ;?>
+							><?php
 						echo get_the_excerpt(); ?></p>
 						<?php } ;?>
-								</div>
-								
-							</div>
-						</div>
-					</div>
-
-							<?php endwhile; ?>
-						<?php wp_reset_postdata(); ?>
-
-			<?php else : ?>
-				<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-			<?php endif; ?>
-
-<!-- END  POST 2 -->
-
-
-
-
-					<div class="row">
-						<div class="col-xs-12">
-							<div class="wpg-post-box wpg-bg-orange-twitter" style="background-color: #dd6e1d; color: #fff;">
-								<p class="wpg-post-box_tag">Social</p>
-								<i class="fa fa-twitter"></i>
-								
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet condi mentum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque egestas nulla ac vestibulum hendrerit.</p>
-								<p><a href="http://www.ghjjjd.com">http://www.ghjjjd.com</a></p>
-							
-							</div>
-						</div>
 					</div>
 				</div>
-				<!-- / Module full width vertical 2 blocks with post + block with twitter post (New Content) -->
-
-
 			</div>
-			
+		</div>
+		<?php endwhile; ?>
+		<?php wp_reset_postdata(); ?>
+		<?php else : ?>
+		<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+		<?php endif; ?>
+		<!-- END  POST 2 -->
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="wpg-post-box wpg-bg-orange-twitter" style="background-color: #dd6e1d; color: #fff;">
+					<p class="wpg-post-box_tag">Social</p>
+					<i class="fa fa-twitter"></i>
+					
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet condi mentum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque egestas nulla ac vestibulum hendrerit.</p>
+					<p><a href="http://www.ghjjjd.com">http://www.ghjjjd.com</a></p>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- / Module full width vertical 2 blocks with post + block with twitter post (New Content) -->
+</div>
