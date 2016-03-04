0f5413dbcbd378c1c4df51b8c4cbc99a2245fa49
Tobiasz Gerczuk <kontakt@tobit.com.pl>
2016-03-04 08:38:58 +0100

    search.php

--- wp-content/themes/majedie/search.php
+++ wp-content/themes/majedie/search.php
@@ -22,21 +22,21 @@ get_header(); ?>
                         $args = array(
                             'post_type'        => 'post',
                             'post_status'      => 'publish',
-                            'posts_per_page'   => $how_many_posts_num,
+                            'posts_per_page'   => 10,
                             'orderby'          => 'date',
                             'order'            => 'DESC',
                             'paged' => $paged,
 
 
 						);
-						$custom_query = new WP_Query ( $query_args );
+						$custom_query = new WP_Query ($args );
 							?>
 
 				<?php while (have_posts()) : the_post(); ?>
 
 
 				<div class="wpg-short-post">
-					<h3><a href="#"><?php echo $i; ?><?php echo get_the_title(); ?></a></h3>
+					<h3><a href="#"><?php echo get_the_title(); ?></a></h3>
 					<p><?php echo wp_trim_words(get_post($post)->post_content, 20, '') ;?></p>
 				</div>
 
