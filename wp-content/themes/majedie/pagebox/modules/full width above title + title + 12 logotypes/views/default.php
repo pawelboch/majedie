32bedbd5778684e04be30a563e64b3b01b523644
wojciechlampa <w.lampa@designcrown.co.uk>
2016-03-04 11:08:35 +0100

    module update

--- wp-content/themes/majedie/pagebox/modules/full width above title + title + 12 logotypes/views/default.php
+++ wp-content/themes/majedie/pagebox/modules/full width above title + title + 12 logotypes/views/default.php
@@ -1,7 +1,18 @@
 <div class="module-wpg module-full-width-above-title-title-12-logotypes"
-	<?php if($this->get('title') == '') {
-		echo 'style="padding-top: 0px;"';
-	}else{echo 'style="padding-bottom: 0px;"';}?>
+	<?php
+	if($this->get('background_color')) {
+		echo 'style="';
+		echo 'background-color: ' . $this->get('background_color') . ';';
+		if($this->get('title') == '') {
+			echo 'style="padding-top: 0px;"';
+		} else {
+			echo 'style="padding-bottom: 0px;"';
+		}
+	}
+	else {
+		echo '"';
+	}
+	;?>
 	>
 	<div class="container">
 		<?php if($this->get('paragraph') != '') { ;?>
@@ -33,75 +44,19 @@
 		><?php echo $this->get('title') ;?></h2>
 		<?php } ;?>
 		<div class="row">
-			<div class="wpg-list-items-inline-block">
-				
-				<ul>
-					
-					<?php foreach ($this->get('logos') as $logo) : ;?>
-					
-					
-					<?php  $img_src = wp_get_attachment_image_src($logo->logo, 'full');
-					$url = $img_src[0];   ?>
-					
-					<li><a href="<?php echo $logo->url; ?>"><img src="<?php echo $url;  ?>" alt="x"></a></li>
-					<?php endforeach ;?>
-					
-				</ul>
-				<?php if ($this->get('llogos')) : ?>
-				<ul>
-					
-					<?php foreach ($this->get('llogos') as $llogo) : ;?>
-					
-					
-					<?php  $img_src = wp_get_attachment_image_src($llogo->llogo, 'full');
-					$url2 = $img_src[0];   ?>
-					
-					<li><a href="<?php echo $llogo->uurl; ?>">
-						<?php if($url2 != '') : ?>
-					<img src="<?php echo $url2;  ?>" alt="x"></a></li>
-					<?php endif ?>
-					<?php endforeach ;?>
-					
-				</ul>
-				<?php endif ?>
-				<?php if ($this->get('lllogos')) : ?>
-				<ul>
-					
-					<?php foreach ($this->get('lllogos') as $lllogo) : ;?>
-					
-					
-					<?php  $img_src = wp_get_attachment_image_src($lllogo->lllogo, 'full');
-					$url3 = $img_src[0];   ?>
-					
-					<li><a href="<?php echo $lllogo->uuurl; ?>">
-						<?php if($url3 != '') : ?>
-					<img src="<?php echo $url3;  ?>" alt="x"></a></li>
-					<?php endif ?>
-					<?php endforeach ;?>
-					
-				</ul>
-				<?php endif ?>
-				<?php if ($this->get('llllogos')) : ?>
-				<ul>
-					
-					<?php foreach ($this->get('llllogos') as $llllogo) : ;?>
-					
-					
-					<?php  $img_src = wp_get_attachment_image_src($llllogo->llllogo, 'full');
-					$url4 = $img_src[0];   ?>
-					
-					<li><a href="<?php echo $llllogo->uuuurl; ?>">
-						<?php if($url4 != '') : ?>
-					<img src="<?php echo $url4;  ?>" alt="x"></a></li>
-					<?php endif ?>
-					<?php endforeach ;?>
-					
-				</ul>
-				<?php endif ?>
-				
+			<?php foreach ($this->get('logotypes') as $logotype) { ?>
+			<div class="col-md-2">
+				<?php
+				if($logotype->url != '') {
+					echo '<a target="_blank" href="' . $logotype->url . '">';
+							};
+							echo '<img src="' . wp_get_attachment_url($logotype->logo) . '"/>';
+							if($logotype->url != '') {
+					echo '</a>';
+				};
+				?>
 			</div>
-			
-			
+			<?php } ;?>
 		</div>
 	</div>
 </div>
