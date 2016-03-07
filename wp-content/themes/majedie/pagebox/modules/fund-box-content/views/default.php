4b8e5eeb0ec912f5701d6e680f8bc1af1e3e155d
Szymek <karmonek@gmail.com>
2016-03-07 11:57:22 +0100

    Add fund box psot

--- wp-content/themes/majedie/pagebox/modules/fund-box-content/views/default.php
+++ wp-content/themes/majedie/pagebox/modules/fund-box-content/views/default.php
@@ -1,19 +1,19 @@
 <div class="module-wpg module-fund-box-content" style="<?php echo ($this->get('background_color')) ? ('background-color: ' . $this->get('background_color') . ';') : '' ?>">
 	<div class="container">
 		<?php if($this->get('title') != '') { ;?>
-		<h1 style="<?php echo ($this->get('title_size')) ? ('font-size: ' . $this->get('title_size') . 'px;') : ''; ?>
+		<h2 style="<?php echo ($this->get('title_size')) ? ('font-size: ' . $this->get('title_size') . 'px;') : ''; ?>
 				   <?php echo ($this->get('title_color')) ? ('color: ' . $this->get('title_color') . ';') : ''; ?>
 		">
 			<?php echo $this->get('title') ;?>
-		</h1>
+		</h2>
 		<?php } ;?>
 
 		<?php if($this->get('content') != '') { ;?>
-			<h1 style="<?php echo ($this->get('content_size')) ? ('font-size: ' . $this->get('content_size') . 'px;') : ''; ?>
+			<p style="<?php echo ($this->get('content_size')) ? ('font-size: ' . $this->get('content_size') . 'px;') : ''; ?>
 			<?php echo ($this->get('content_color')) ? ('color: ' . $this->get('content_color') . ';') : ''; ?>
 				">
 				<?php echo $this->get('content') ;?>
-			</h1>
+			</p>
 		<?php } ;?>
 	</div>
 </div>
---
+++ wp-content/themes/majedie/pagebox/modules/fund-box-post/views/default.php
@@ -0,0 +1,19 @@
+<?php
+	$post = get_post($this->get('postID'));
+?>
+
+<div class="module-wpg module-fund-box-post">
+	<div class="container">
+		<div class="wpg-post-box">
+			<?php
+				$category = get_the_terms( $post->ID, 'category' );
+				$firstCategory = ($category) ? $category[0]->name : '';
+			?>
+			<p class="wpg-tags"><?php echo $firstCategory; ?></p>
+			<h3><a href="<?php echo get_permalink($post->ID); ?>"><?php echo $post->post_title; ?></a></h3>
+			<p class="wpg-date"><?php echo mysql2date('j / m / Y', $post->post_date); ?></p>
+
+			<p><?php echo $post->post_excerpt; ?></p>
+		</div>
+	</div>
+</div>
