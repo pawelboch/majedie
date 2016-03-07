6adb18f181ab16e9ac39f2a271c5837fc683454e
Szymek <karmonek@gmail.com>
2016-03-07 14:22:20 +0100

    Add fund horizontal menu repeater

--- wp-content/themes/majedie/pagebox/modules/fund-box-nav-menu/views/default.php
+++ wp-content/themes/majedie/pagebox/modules/fund-box-nav-menu/views/default.php
@@ -1,12 +1,10 @@
 <div class="module-wpg module-fund-box-nav-menu">
 	<div class="container clearfix">
-			<ul class="clearfix">
-				<li class="wpg-fund-menu-active"><a href="#">Overview</a></li>
-				<li><a href="#">Factsheets</a></li>
-				<li><a href="#">KIIDs</a></li>
-				<li><a href="#">Prices</a></li>
-				<li><a href="#">Charges</a></li>
-				<li><a href="#">Commentary</a></li>
-			</ul>
+		<ul class="clearfix">
+			<?php foreach($this->get('menu-items') as $item): ?>
+				<li class=""><a href="<?php echo $item->link_url; ?>"><?php echo $item->link_text; ?></a></li>
+				<!--		<li class="wpg-fund-menu-active	">		-->
+			<?php endforeach; ?>
+		</ul>
 	</div>
 </div>
