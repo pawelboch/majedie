03e8023608a9dc47bfdbd761dfc7991212e3b7dc
alexns1518 <alexp@nurtureagency.com>
2016-03-07 16:52:44 +0100

    up

--- wp-content/themes/majedie/pagebox/modules/full width funds info/views/default.php
+++ wp-content/themes/majedie/pagebox/modules/full width funds info/views/default.php
@@ -1,3 +1,191 @@
-<div class="module-wpg module_full_width_funds_info">
+<div class="module-wpg module-full-width-funds-info">
+	<div class="wpg-container-tabels">
 
+		<div class="container">
+
+			<div class="wpg-table-wrap-outset">
+				<div class="wpg-table-wrap">
+					<table>
+						<caption>Platforms</caption>
+						<thead>
+							<tr>
+								<th>FUND</th>
+								<th>UK EQUITY<br>FUND</th>
+								<th>UK INCOME<br>FUND</th>
+								<th>UK FOCUS<br>FUND</th>
+								<th>GLOABL EQUITY<gr>FUND</th>
+								<th>GLOABL EQUITY<gr>FUND</th>
+								<th>US EQUITY<br>FUND</th>
+							</tr>
+						</thead>
+
+
+							<tbody>
+							<tr>
+							<th>Aegon Wrap</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr>
+							<th>Ascentric</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr>
+							<th>AXA Wealth Elevate</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th>Barclays Stockbrokers</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th>Bestinvest</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th>Cavendish Online</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th>Charles Stanley Direct</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th>Cofunds Institutional</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th>Fidelity</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th>FNZ</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th>Hargreaves Lansdown</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							</tr><tr><th>James Hay</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							</tr><tr><th>Nexus</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th><i class="fa fa-times"></i>via</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th>Nucleus</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th>Old Mutual</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th>Raymond James</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th>Sippcentre</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th>Standard Life</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th>TD Direct</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr><tr><th>Transact</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							</tr><tr><th>Zurich</th>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-check"></i></td>
+							<td><i class="fa fa-times"></i></td>
+							</tr>
+						</tbody>
+
+					</table>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
 </div>
