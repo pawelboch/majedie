bdf0200faffc97745043f6f01a07cd66c1127851
wojciechlampa <w.lampa@designcrown.co.uk>
2016-03-14 09:40:46 +0100

    update

--- wp-content/themes/majedie/pagebox/templates/100, 33-66, 100/template.php
+++ wp-content/themes/majedie/pagebox/templates/100, 33-66, 100/template.php
@@ -1,12 +1,9 @@
 <div class="module-wpg module-full-width-fund">
-	
 	<?php foreach ($this->get_variable( 'top_modules' ) as $module): ?>
 	<?php $module->display(); ?>
 	<?php endforeach ?>
-
 	<div class="container">
 		<div class="row">
-
 			<div class="col-xs-12 col-md-4 wpg-inset-col">
 				<div class="wpg-inset-box-2">
 					<?php foreach ($this->get_variable( 'left_modules' ) as $module): ?>
@@ -14,7 +11,6 @@
 					<?php endforeach ?>
 				</div>
 			</div>
-
 			<div class="col-xs-12 col-md-8 wpg-inset-col">
 				<div class="wpg-inset-box-1">
 					<?php foreach ($this->get_variable( 'right_modules' ) as $module): ?>
@@ -22,12 +18,9 @@
 					<?php endforeach ?>
 				</div>
 			</div>
-
 		</div>
 	</div>
-	
 	<?php foreach ($this->get_variable( 'bottom_modules' ) as $module): ?>
 	<?php $module->display(); ?>
 	<?php endforeach ?>
-
 </div>
