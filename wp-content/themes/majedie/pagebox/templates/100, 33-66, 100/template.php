ceab1a9770bcf4b42e240a95b23096cd5d52c9b8
wojciechlampa <w.lampa@designcrown.co.uk>
2016-03-14 09:25:55 +0100

    update

--- wp-content/themes/majedie/pagebox/templates/100, 33-66, 100/template.php
+++ wp-content/themes/majedie/pagebox/templates/100, 33-66, 100/template.php
@@ -9,7 +9,7 @@
 
 			<div class="col-xs-12 col-md-4 wpg-inset-col">
 				<div class="wpg-inset-box-2">
-					<?php foreach ($this->get_variable( 'left-first_modules' ) as $module): ?>
+					<?php foreach ($this->get_variable( 'left_modules' ) as $module): ?>
 					<?php $module->display(); ?>
 					<?php endforeach ?>
 				</div>
@@ -17,7 +17,7 @@
 
 			<div class="col-xs-12 col-md-8 wpg-inset-col">
 				<div class="wpg-inset-box-1">
-					<?php foreach ($this->get_variable( 'right-first_modules' ) as $module): ?>
+					<?php foreach ($this->get_variable( 'right_modules' ) as $module): ?>
 					<?php $module->display(); ?>
 					<?php endforeach ?>
 				</div>
