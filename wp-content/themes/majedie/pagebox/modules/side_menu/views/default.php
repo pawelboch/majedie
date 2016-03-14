cd3702f909821ffeda8a9e2fbbb3a3c5457eac95
wojciechlampa <w.lampa@designcrown.co.uk>
2016-03-14 09:40:09 +0100

    update

--- wp-content/themes/majedie/pagebox/modules/side_menu/views/default.php
+++ wp-content/themes/majedie/pagebox/modules/side_menu/views/default.php
@@ -14,7 +14,6 @@
 	text-decoration: none;
 	color: #000;
 	font-family: Avenir-Black;
-
 }
 .side_menu .active{
 	color:#000;
@@ -22,69 +21,48 @@
 }
 .side_menu{
 margin-top:60px;
-
 }
-
 .side_menu a:visited{
 	color: #000;
 }
 </style>
-
-<?php 
+<?php
 global $post;
 $la = get_permalink($post->ID);
-
- ?>
-<div class="side_menu">xxxxxxx
+?>
+<div class="side_menu">
 	<div class="wpg-inset-col">
-				<div class="wpg-inset-box-2">
-
-
-					<div class="wpg-box-fund">
-						<ul class="wpg-simple-list tg_simple">
-								<?php foreach ($this->get('items') as $item) {
-								
-								$page = get_post($item->post->page);
-								$li = get_permalink($page->ID);
-
-								if($li == $la){
-									$active = 'class="active"';
-								}
-								else{
-									$active = '';
-								}
-
-									$page2 = get_post($item->post->post);
-									$li2 = get_permalink($page2->ID);
-									if($li2 == $la){
-										$active2 = 'class="active"';
-									}
-									else{
-										$active = '';
-									}
-							
-								
-								if($item->post->type == "page"){
-									
-									echo "<li><a ".$active." href='".get_permalink($page->ID)."'>".$page->post_title."</a></li>";
-						
-								}
-								else{
-									$page = get_post($item->post->post);
-										echo "<li><a ".$active2." href='".get_permalink($page->ID)."'>".$page->post_title."</a></li>";
-					
-
-						
-								}
-
-									 ?></li>
-								<?php } ?>
-								
-						</ul>
-					</div>	
-
-
-				</div>
-				</div>
-
+		<div class="wpg-inset-box-2">
+			<div class="wpg-box-fund">
+				<ul class="wpg-simple-list tg_simple">
+					<?php foreach ($this->get('items') as $item) {
+					$page = get_post($item->post->page);
+					$li = get_permalink($page->ID);
+					if($li == $la){
+						$active = 'class="active"';
+					}
+					else{
+						$active = '';
+					}
+						$page2 = get_post($item->post->post);
+						$li2 = get_permalink($page2->ID);
+						if($li2 == $la){
+							$active2 = 'class="active"';
+						}
+						else{
+							$active = '';
+						}
+					if($item->post->type == "page"){
+						echo "<li><a ".$active." href='".get_permalink($page->ID)."'>".$page->post_title."</a></li>";
+					}
+					else{
+						$page = get_post($item->post->post);
+						echo "<li><a ".$active2." href='".get_permalink($page->ID)."'>".$page->post_title."</a></li>";
+					}
+				?></li>
+				<?php } ?>
+			</ul>
+		</div>
+	</div>
+</div>
 </div>
